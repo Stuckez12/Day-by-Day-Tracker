422bf6289e5d2431dc2b0fbb5f3fa0f60f9cf499
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2026-03-01 16:11:20 +0000

    HTTPS Setup (#10)
    * working https?
    
    * Restart services unless stopped
    
    * Cleanup

--- frontend/src/scripts/api.ts
+++ frontend/src/scripts/api.ts
@@ -1,6 +1,7 @@
 const HOST = import.meta.env.VITE_BACKEND_HOST;
+const PORT = import.meta.env.VITE_BACKEND_PORT;
 
-const BASE_URL = `http://${HOST}/api/v1`;
+const BASE_URL = `https://${HOST}:${PORT}/api/v1`;
 
 class APICalls {
   base_url: string;
