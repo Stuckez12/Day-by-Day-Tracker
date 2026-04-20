cf9673346ed53238de003d66cf39e7a88bff150f
Stuckez12 <jake.stuckey46@gmail.com>
2026-04-20 22:18:00 +0100

    Added toggle for navbar

--- frontend/src/components/common/navbar/NavBar.tsx
+++ frontend/src/components/common/navbar/NavBar.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PageWrapper from "components/common/PageWrapper";
 import NavBarButton from "components/common/navbar/NavBarButton.tsx";
 import Message from "components/Message";
+import MenuSVG from "components/svg/menu.tsx"
 
 import LandingPage from "pages/LandingPage";
 import PersonnelPage from "pages/PersonnelPage";
@@ -10,17 +12,27 @@ import RankingPage from "pages/RankingPage";
 
 import "styles/navbar.scss";
 
+
+
 function NavBar() {
+  const [navbarState, setNavbarState] = useState<boolean>(false);
+
+  function toggle_navbar() {
+    if (navbarState) setNavbarState(false);
+    else setNavbarState(true);
+  }
+
   return (
     <Router>
       <nav id="navigation-bar">
         <PageWrapper>
-          <ul className="nav-list">
+          <ul className={`nav-list ${navbarState ? "dropdown" : ""}`}>
             <NavBarButton frontend_url="/" display_text="Tracker" />
             <NavBarButton frontend_url="/rates" display_text="Rates" />
             <NavBarButton frontend_url="/tasks" display_text="Tasks" />
             <NavBarButton frontend_url="/stats" display_text="Stats" />
             <NavBarButton frontend_url="/personnel" display_text="Personnel" right_hand_side={true}/>
+            <li className="nav-toggle right" onClick={() => toggle_navbar()}><MenuSVG/></li>
           </ul>
         </PageWrapper>
       </nav>
---
+++ frontend/src/components/svg/menu.tsx
@@ -0,0 +1,13 @@
+function MenuSVG() {
+    return (
+        <svg width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+            <rect x="8" y="11" width="24" height="3" rx="1.5" fill="white"/>
+            <rect x="8" y="18.5" width="24" height="3" rx="1.5" fill="white"/>
+            <rect x="8" y="26" width="24" height="3" rx="1.5" fill="white"/>
+        </svg>
+    );
+}
+
+export default MenuSVG;
+
+
