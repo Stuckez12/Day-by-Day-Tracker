72fb2a076e2de057e94ec8ab881365d32f508d02
Stuckez12 <jake.stuckey46@gmail.com>
2026-04-24 19:40:04 +0100

    Service tests

--- tests/api/services/test_auth_service.py
+++ tests/api/services/test_auth_service.py
@@ -1,19 +1,18 @@
 import pytest
 
+from pytest_mock import MockerFixture
 from sqlalchemy.orm import Session
 
 from src.common.password_hash import pwd_hash
-from src.models import PersonalModel, RankerModel
-from src.schemas import (
-    CreatePersonnelRequest,
-    UpdatePersonnelRequest,
-    InvalidSchema,
-)
+from src.models import PersonalModel
+from src.schemas import CreatePersonnelRequest, LogInRequest
 from src.services import AuthService
+
+from tests.api.constants import VALID_PASSWORD
 
 
 class TestAuthService:
-    def test_register_personnel_success(
+    def test_register_personnel(
         self, test_session: Session, test_auth_service: AuthService
     ):
         data = CreatePersonnelRequest(
@@ -34,3 +33,64 @@ class TestAuthService:
         finally:
             test_session.delete(personnel)
             test_session.commit()
+
+    def test_register_personnel_email_already_in_use(
+        self, test_auth_service: AuthService, test_personnel: PersonalModel
+    ):
+        data = CreatePersonnelRequest(
+            email=test_personnel.email,
+            password="Password1.",
+            first_name="Test",
+            last_name="User",
+        )
+
+        with pytest.raises(ValueError, match="Email already in use"):
+            test_auth_service.register(data)
+
+    def test_register_personnel_password_hashing_fails(
+        self, mocker: MockerFixture, test_auth_service: AuthService
+    ):
+        mocker.patch.object(pwd_hash, "hash", side_effect=ValueError("Forced Error"))
+
+        data = CreatePersonnelRequest(
+            email="email@email.com",
+            password="Password1.",
+            first_name="Test",
+            last_name="User",
+        )
+
+        with pytest.raises(
+            ValueError, match="Unable to hash password. Please try again"
+        ):
+            test_auth_service.register(data)
+
+    def test_log_in_personnel(
+        self, test_auth_service: AuthService, test_personnel: PersonalModel
+    ):
+        data = LogInRequest(
+            email=test_personnel.email,
+            password=VALID_PASSWORD,
+        )
+
+        result = test_auth_service.log_in(data)
+        assert result == test_personnel
+
+    def test_log_in_personnel_invalid_email(self, test_auth_service: AuthService):
+        data = LogInRequest(
+            email="invalid@email.com",
+            password=VALID_PASSWORD,
+        )
+
+        with pytest.raises(ValueError, match="Invalid email or password"):
+            test_auth_service.log_in(data)
+
+    def test_log_in_personnel_invalid_password(
+        self, test_auth_service: AuthService, test_personnel: PersonalModel
+    ):
+        data = LogInRequest(
+            email=test_personnel.email,
+            password="invalid_password",
+        )
+
+        with pytest.raises(ValueError, match="Invalid email or password"):
+            test_auth_service.log_in(data)
