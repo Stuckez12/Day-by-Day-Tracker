0038c75e37fc63f2963ada6d791c57d5ede50207
Stuckez12 <jake.stuckey46@gmail.com>
2026-05-25 15:42:07 +0100

    Dont actually run the command

--- tests/api/tasks/maintenance/test_database_backup_task.py
+++ tests/api/tasks/maintenance/test_database_backup_task.py
@@ -10,15 +10,12 @@ from src.tasks import database_backup
 
 
 class TestDatabaseBackupTask:
-    def test_backup_success(self, celery_worker: None):
+    def test_backup_success(self, mocker: MockerFixture, celery_worker: None):
+        mocker.patch.object(subprocess, "run", return_value=None)
         task: AsyncResult = database_backup.delay()
         assert BackupSchema.model_validate(task.result)
 
-    def test_backup_command_failure(
-        self,
-        mocker: MockerFixture,
-        celery_worker: None,
-    ):
+    def test_backup_command_failure(self, mocker: MockerFixture, celery_worker: None):
         mock_error = subprocess.CalledProcessError(
             returncode=1, cmd="command", output="", stderr="error"
         )
