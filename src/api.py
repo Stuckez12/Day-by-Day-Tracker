36a841383c6ecac86d09990675844903221d6b22
Stuckez12 <jake.stuckey46@gmail.com>
2026-05-25 09:48:41 +0100

    Route to trigger tasks

--- src/api.py
+++ src/api.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from src.routes import auth, personal, ranking, task
+from src.routes import auth, personal, ranking, task, task_run
 
 api = APIRouter()
 api.include_router(auth.api)
 api.include_router(task.api)
 api.include_router(personal.api)
 api.include_router(ranking.api)
+api.include_router(task_run.api)
