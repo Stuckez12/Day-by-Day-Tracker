b5961fa938fdf4a8aa5d0b1e8e2561b198aef89c
Stuckez12 <81242118+Stuckez12@users.noreply.github.com>
2026-09-11 11:05:03 +0100

    Dev/Test Banner (#63)

--- frontend/app/(account)/layout.tsx
+++ frontend/app/(account)/layout.tsx
@@ -1,63 +1,22 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { useContext, useEffect } from "react";
-import { useSession } from "next-auth/react";
-
-import { getPersonnelQuery } from "@/lib/queries/personnel";
-import { getAccessToken } from "@/lib/common/auth/getAccessToken";
-
-import { PartialPersonnelContext } from "@/components/common/contexts/personnelContext";
 import NavBar from "@/components/navigation/NavBar";
+import ClientAccountLayout from "./ClientAccountLayout";
+
+export const dynamic = "force-dynamic";
 
 export default function AccountGroupLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const router = useRouter();
-  const { data: _session, status } = useSession();
-  const { setPartialPersonnel } = useContext(PartialPersonnelContext);
-
-  useEffect(() => {
-    async function loadPersonnel() {
-      if (status !== "authenticated") {
-        return;
-      }
-
-      const accessToken = await getAccessToken();
-
-      if (!accessToken) {
-        router.replace("/login");
-        return;
-      }
-
-      const userResult = await getPersonnelQuery();
-
-      if (!userResult.ok) {
-        router.push("/login");
-        return;
-      }
-
-      setPartialPersonnel(userResult.data);
-    }
-
-    if (status === "unauthenticated") {
-      router.replace("/login");
-      return;
-    }
-
-    loadPersonnel();
-  }, [setPartialPersonnel, router, status]);
-
-  if (status !== "authenticated") {
-    return <></>;
-  }
+  let banner = "";
+  if (process.env.NODE_ENV == "development")
+    banner = "Application is in development mode";
+  else if (process.env.IS_TEST_PROD)
+    banner = "Application is deployed only as a showcase";
 
   return (
-    <div className="flex flex-col">
-      <NavBar />
-      <main>{children}</main>
-    </div>
+    <ClientAccountLayout nav={<NavBar pageBanner={banner} />}>
+      {children}
+    </ClientAccountLayout>
   );
 }
--- frontend/components/navigation/NavBar.tsx
+++ frontend/components/navigation/NavBar.tsx
@@ -1,20 +1,28 @@
+import PageBanner from "../common/banners/PageBanner";
 import PageWrapper from "../common/PageWrapper";
 import NavBarItem from "./NavBarItem";
 
-export default function NavBar() {
+interface NavBarProps {
+  pageBanner?: string;
+}
+
+export default function NavBar({ pageBanner = "" }: NavBarProps) {
   return (
-    <nav className="bg-secondary mb-4 py-1">
-      <PageWrapper>
-        <div className="flex flex-row w-full">
-          <div className="mr-auto flex flex-row space-x-1">
-            <NavBarItem name="Tracker" urlPath="/tracker" />
-            <NavBarItem name="Rankings" urlPath="/ranking" />
+    <div className="mb-4">
+      <nav className="bg-secondary py-1">
+        <PageWrapper>
+          <div className="flex flex-row w-full">
+            <div className="mr-auto flex flex-row space-x-1">
+              <NavBarItem name="Tracker" urlPath="/tracker" />
+              <NavBarItem name="Rankings" urlPath="/ranking" />
+            </div>
+            <div className="flex flex-row space-x-1">
+              <NavBarItem name="Personnel" urlPath="/personnel" />
+            </div>
           </div>
-          <div className="flex flex-row space-x-1">
-            <NavBarItem name="Personnel" urlPath="/personnel" />
-          </div>
-        </div>
-      </PageWrapper>
-    </nav>
+        </PageWrapper>
+      </nav>
+      <PageBanner message={pageBanner ? pageBanner : ""} type="INFO" />
+    </div>
   );
 }
